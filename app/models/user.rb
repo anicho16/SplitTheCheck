527f6db41ee2f9ceccc30c0168dafd7bac7ea825
Amber Nicholas <anicho16@my.westga.edu>
2023-04-15 10:09:08 -0500

    In nav, if the user is logged in sees Add restaurant button. Otherwise, the New User and Login buttons are present. Added method to determine if user is logged in in application helper. Added method to validate formatting of user email and also to determine if user has voted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   validates :username, presence: true
   validates :password, presence: true
-  validates :email, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_many :votes
+
+  def has_voted?(restaurant)
+    votes.exists?(restaurant_id: restaurant.id)
+  end
 end
